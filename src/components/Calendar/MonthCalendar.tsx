a8a82bc6ee43fca6fdfa6028de208196666d20c2
lishengyu <lishengyu@baidu.com>
2024-03-21 18:53:44 +0800

    feat: MonthCalendar组件

---
+++ src/components/Calendar/MonthCalendar.tsx
@@ -0,0 +1,8 @@
+function MonthCalendar() {
+  const weekList = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+  return <div>
+    <div className="calendar-header"></div>
+  </div>;
+}
+
+export default MonthCalendar;
