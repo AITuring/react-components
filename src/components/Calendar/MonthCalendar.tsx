ea436cfadb2d7d183c2d4917c7104682bde0e11c
lishengyu <lishengyu@baidu.com>
2024-03-25 15:48:15 +0800

    feat: 新增简单的日程

--- src/components/Calendar/MonthCalendar.tsx
+++ src/components/Calendar/MonthCalendar.tsx
@@ -14,6 +14,11 @@ interface DayInfo {
   currentMonth: boolean;
 }
 
+interface Schedules {
+  date: Dayjs;
+  schedule: string;
+  type: 'work' | 'travel' | 'other';
+}
 
 function getAllDays(date: Dayjs): Array<DayInfo> {
   const startDate = date.startOf('month');
@@ -49,11 +54,20 @@ function renderDays(
   value: Dayjs,
   selectHandler: MonthCalendarProps['selectHandler'],
 ) {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [schedules, setSchedules] = useState<Schedules[]>([]);
+
+  console.log(schedules);
+
   const rows = [];
   for (let i = 0; i < 6; i++) {
     const row = [];
     for (let j = 0; j < 7; j++) {
       const day = dayInfo[i * 7 + j];
+      const schedule = schedules.find((item) => {
+        return item.date.format('YYYY-MM-DD') === day.date.format('YYYY-MM-DD');
+      });
+
       row[j] = (
         <div
           className={
@@ -61,28 +75,76 @@ function renderDays(
               ? 'w-1/7 border border-gray-300 text-gray-600 overflow-hidden'
               : 'w-1/7 border border-gray-300 text-gray-300 overflow-hidden'
           }
-          onClick={() => console.log(day)}
+          onClick={() => setEdit(true)}
         >
           {dateRender ? (
             dateRender(day.date)
           ) : (
-            <div className="p-3 cursor-pointer"  onClick={e => {
-              e.stopPropagation();
-              selectHandler?.(day.date)
-            }}>
-              <div className={
-                value.format('YYYY-MM-DD') === day.date.format('YYYY-MM-DD')
-                ? "bg-sky-500 text-white w-7 h-7 leading-7 rounded-full text-center cursor-pointer"
-                : "w-7 h-7 leading-7 rounded-full text-center "
-              }>
+            <div
+              className="p-3 cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                selectHandler?.(day.date);
+              }}
+            >
+              <div
+                className={
+                  value.format('YYYY-MM-DD') === day.date.format('YYYY-MM-DD')
+                    ? 'bg-sky-500 text-white w-7 h-7 leading-7 rounded-full text-center cursor-pointer'
+                    : 'w-7 h-7 leading-7 rounded-full text-center '
+                }
+              >
                 {day.date.date()}
               </div>
-              <div
-                className="calendar-month-body-cell-date-content"
-                >
+              <div className="calendar-month-body-cell-date-content">
                 {dateInnerContent?.(day.date)}
               </div>
-              <div>111</div>
+              <div className="mt-2">
+                {edit ? (
+                  <input
+                    aria-label=""
+                    id="input-id"
+                    type="text"
+                    placeholder=""
+                    title="input"
+                    className="w-full h-10 rounded-md border-gray-300 focus:border-sky-500 focus:ring-indigo"
+                    onBlur={(e) => {
+                      setSchedules([
+                        ...schedules,
+                        {
+                          date: day.date,
+                          schedule: e.target.value,
+                          type: 'work',
+                        },
+                      ]);
+                      setEdit(false);
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        const target = e.target as HTMLInputElement;
+                        setSchedules([
+                          ...schedules,
+                          {
+                            date: day.date,
+                            schedule: target.value,
+                            type: 'work',
+                          },
+                        ]);
+                        setEdit(false);
+                      }
+                    }}
+                  />
+                ) : (
+                  schedule ? (
+                    <div className="flex items-baseline">
+                    <div className="w-2 h-2 bg-cyan-400 rounded mr-1"></div>
+                    <div className="text-sm text-slate-500">
+                      {schedule.schedule}
+                    </div>
+                  </div>
+                  ) : null
+                )}
+              </div>
             </div>
           )}
         </div>
@@ -95,11 +157,20 @@ function renderDays(
 
 function MonthCalendar(props: MonthCalendarProps) {
   const localeContext = useContext(LocaleContext);
-  const { value, dateRender, dateInnerContent, selectHandler, curMonth } = props;
+  const { value, dateRender, dateInnerContent, selectHandler, curMonth } =
+    props;
 
   const CalendarLocale = allLocales[localeContext.locale];
 
-  const weekList = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  const weekList = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+  ];
   const allDays = getAllDays(curMonth);
 
   return (
@@ -112,7 +183,15 @@ function MonthCalendar(props: MonthCalendarProps) {
             </div>
           ))}
         </div>
-        <div>{renderDays(allDays, dateRender, dateInnerContent, value, selectHandler)}</div>
+        <div>
+          {renderDays(
+            allDays,
+            dateRender,
+            dateInnerContent,
+            value,
+            selectHandler,
+          )}
+        </div>
       </div>
     </div>
   );
